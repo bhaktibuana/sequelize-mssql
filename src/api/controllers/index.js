eff7246eb27fa3ae8b099c0402853d559dc0deff
bhaktibuana <bhaktibuana19@gmail.com>
2022-11-12 12:18:05 +0700

    crate fruits controller

--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -1,7 +1,9 @@
 const usersController = require("./users.controller");
 const userRoleController = require("./userRole.controller");
+const fruitsController = require("./fruits.controller");
 
 module.exports = {
   usersController,
   userRoleController,
+  fruitsController,
 };
